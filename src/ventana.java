16fec51c430a5fabf339631f60f72d1f49b98625
FMR08 <135428100+FMR08@users.noreply.github.com>
2023-11-08 10:22:10 -0300

    Update ventana.java

--- src/ventana.java
+++ src/ventana.java
@@ -1,29 +1,17 @@
 
 import javax.swing.*;
 import java.awt.*;
-import javax.swing.ImageIcon;
+
 public class ventana extends JFrame {
-    public JPanel panel;
-    private monButton mon = new monButton();
-    private ButtonBeber drink = new ButtonBeber();
-    private PanelPrincipal pan = new PanelPrincipal(new Moneda1000());
-
+    private PanelPrincipal panel =new PanelPrincipal();
     ventana(){
-        this.setLayout(new BorderLayout());
-        this.add(mon,BorderLayout.NORTH);
-        this.add(pan,BorderLayout.CENTER);
-        this.add(drink,BorderLayout.SOUTH);
 
+        this.add(panel);
         this.setSize(500, 330);
-
         this.setVisible(true);
         this.setTitle("Tarea_3");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-    private void Boton(){
-        JButton mon = new JButton();
-        mon.setBounds(100,100,100,100);
-        panel.add(mon);
-    }
+
 
 }
