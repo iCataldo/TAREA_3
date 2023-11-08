a46d85c40d00ba995bd5ac81580d16bf44f878a0
Ignacio Cataldo <146041107+iCataldo@users.noreply.github.com>
2023-11-08 15:30:40 -0300

    Update PanelMatriz.java

--- src/PanelMatriz.java
+++ src/PanelMatriz.java
@@ -6,15 +6,27 @@ public class PanelMatriz extends JPanel {
         FlowLayout izquierda = new FlowLayout(FlowLayout.LEFT);
         this.setLayout(izquierda);
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(5,2));
-        panel.add(new ButtonBebida_Coca(exp,mon));
-        panel.add(new ButtonBebida_Sprite(exp,mon));
-        panel.add(new ButtonBebida_Snicker(exp,mon));
-        panel.add(new ButtonBebida_Super_8(exp,mon));
-        panel.add(new JButton("Nada_0"));
-        panel.add(new JButton("Nada_1"));
-        panel.add(new JButton("Nada_2"));
-        panel.add(new JButton("Nada_3"));
+        panel.setLayout(new GridLayout(2,2));
+        
+        //BOTÓN COCA COLA
+        ImageIcon image1 = new ImageIcon("iconococa.png");
+        ButtonBebida_Coca botoncoca = new ButtonBebida_Coca(exp, mon, image1);
+        panel.add(botoncoca);
+
+        //BOTÓN SPRITE
+        ImageIcon image2 = new ImageIcon("iconosprite.png");
+        ButtonBebida_Sprite botonsprite = new ButtonBebida_Sprite(exp, mon, image2);
+        panel.add(botonsprite);
+
+        //BOTÓN SNICKERS
+        ImageIcon image3 = new ImageIcon("iconosnickers.png");
+        ButtonBebida_Snicker botonsnicker = new ButtonBebida_Snicker(exp, mon, image3);
+        panel.add(botonsnicker);
+
+        //BOTÓN SUPER8
+        ImageIcon image4 = new ImageIcon("iconosuper8.png");
+        ButtonBebida_Super_8 botonsuper8 = new ButtonBebida_Super_8(exp, mon, image4);
+        panel.add(botonsuper8);
         this.add(panel);
     }
 }
