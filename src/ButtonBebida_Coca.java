092072cd3d8d3f7f265d9cac8e18fcfdca49c212
FMR08 <135428100+FMR08@users.noreply.github.com>
2023-11-08 12:58:18 -0300

    Update ButtonBebida_Coca.java

--- src/ButtonBebida_Coca.java
+++ src/ButtonBebida_Coca.java
@@ -6,27 +6,29 @@ public class ButtonBebida_Coca extends JButton {
     private Expendedor exp =new Expendedor(3);
     private Moneda mon;
     private Comprador com;
-    public ButtonBebida_Coca(Expendedor exp, int m){
+    public ButtonBebida_Coca(Expendedor exp, monButton m){
         super("Coca");
         this.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    com=new Comprador(new Moneda500(),exp.COCA,exp);
-                } catch (NoHayProductoException ex) {
-                    throw new RuntimeException(ex);
-                } catch (PagoInsuficienteException ex) {
-                    throw new RuntimeException(ex);
-                } catch (PagoIncorrectoException ex) {
-                    throw new RuntimeException(ex);
-                }
-                if (com.queBebiste()!=null){
-                    System.out.println("Coca Cola");
+
+                if (Expendedor.PRODUCTOS.PreCoca.precio<=m.getM()){
+
+                    try {
+                        com=new Comprador(new Moneda1000(),exp.COCA,exp);
+                    } catch (NoHayProductoException ex) {
+                        throw new RuntimeException(ex);
+                    } catch (PagoInsuficienteException ex) {
+                        throw new RuntimeException(ex);
+                    } catch (PagoIncorrectoException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    if (com!=null) {
+                        m.reducirMoneda(Expendedor.PRODUCTOS.PreCoca.precio);
+                        System.out.println("Coca Cola");
+                    }
                 }
             }
         });
     }
-
-
-
 }
