00bfc620f76442f815a8bbbd760a9237c5dd9c09
FMR08 <135428100+FMR08@users.noreply.github.com>
2023-11-08 13:00:33 -0300

    Update PanelMatriz.java

--- src/PanelMatriz.java
+++ src/PanelMatriz.java
@@ -2,15 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 
 public class PanelMatriz extends JPanel {
-    public PanelMatriz(Expendedor exp, PanelMoneda mon){
+    public PanelMatriz(Expendedor exp, monButton mon){
         FlowLayout izquierda = new FlowLayout(FlowLayout.LEFT);
         this.setLayout(izquierda);
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(4,2));
-        panel.add(new ButtonBebida_Coca(exp,mon.getM()));
-        panel.add(new ButtonBebida_Coca(exp, mon.getM()));
-        panel.add(new ButtonBebida_Coca(exp, mon.getM()));
-        panel.add(new ButtonBebida_Coca(exp, mon.getM()));
+        panel.setLayout(new GridLayout(5,2));
+        panel.add(new ButtonBebida_Coca(exp,mon));
+        panel.add(new ButtonBebida_Sprite(exp,mon));
+        panel.add(new ButtonBebida_Snicker(exp,mon));
+        panel.add(new ButtonBebida_Super_8(exp,mon));
         panel.add(new JButton("Nada_0"));
         panel.add(new JButton("Nada_1"));
         panel.add(new JButton("Nada_2"));
