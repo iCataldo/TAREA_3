8ba012ac6c025ab213ae83e7aa6a4e21562eac30
Ignacio Cataldo <146041107+iCataldo@users.noreply.github.com>
2023-11-08 10:43:49 -0300

    Update monButton.java

--- src/monButton.java
+++ src/monButton.java
@@ -5,19 +5,22 @@ import java.awt.event.ActionListener;
 
 public class monButton extends JPanel {
     int m=0;
+    private JLabel contador = new JLabel();
     DepositoM monedas = new DepositoM();
     private JPanel panel = new JPanel();
     public monButton(){
         JButton mon = new JButton("moneda");
         mon.addActionListener(new SumarMoneda());
-
         this.add(mon);
+        contador.setText("$"+m);
+        this.add(contador);
     }
     private class SumarMoneda implements ActionListener {
         public void actionPerformed(ActionEvent ae) {
             monedas.addMoneda();
             m = m +100;
-            System.out.println(m);
+            contador.setText("$"+String.valueOf(m));
+            System.out.println("$"+m);
         }
     }
 
