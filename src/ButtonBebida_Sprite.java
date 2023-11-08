01b356ac0845d0054af19422480169689af291ac
FMR08 <135428100+FMR08@users.noreply.github.com>
2023-11-08 13:02:28 -0300

    Add files via upload

---
+++ src/ButtonBebida_Sprite.java
@@ -0,0 +1,37 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+public class ButtonBebida_Sprite extends JButton {
+
+    private Expendedor exp =new Expendedor(3);
+    private Moneda mon;
+    private Comprador com;
+    public ButtonBebida_Sprite(Expendedor exp, monButton m){
+        super("Sprite");
+        this.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if (Expendedor.PRODUCTOS.PreSprite.precio<=m.getM()){
+
+                    try {
+                        com=new Comprador(new Moneda1000(),exp.SPRITE,exp);
+                    } catch (NoHayProductoException ex) {
+                        throw new RuntimeException(ex);
+                    } catch (PagoInsuficienteException ex) {
+                        throw new RuntimeException(ex);
+                    } catch (PagoIncorrectoException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    if (com!=null) {
+                        m.reducirMoneda(Expendedor.PRODUCTOS.PreSprite.precio);
+                        System.out.println("Sprite");
+                    }
+                }
+            }
+        });
+    }
+
+
+
+}
