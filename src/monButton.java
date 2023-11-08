d38bc91c543895e6591f73daaf1c0883f3f1841a
FMR08 <135428100+FMR08@users.noreply.github.com>
2023-11-08 12:59:54 -0300

    Update monButton.java

--- src/monButton.java
+++ src/monButton.java
@@ -9,21 +9,32 @@ public class monButton extends JPanel {
     DepositoM monedas = new DepositoM();
     private JPanel panel = new JPanel();
     public monButton(){
+        setLayout(new FlowLayout(FlowLayout.RIGHT));
         JButton mon = new JButton("moneda");
         mon.addActionListener(new SumarMoneda());
-        this.add(mon);
+        contador.setFont(new Font("arial",Font.PLAIN,20));
         contador.setText("$"+m);
         this.add(contador);
+        this.add(mon);
     }
     private class SumarMoneda implements ActionListener {
         public void actionPerformed(ActionEvent ae) {
-            monedas.addMoneda();
-            m = m +100;
-            contador.setText("$"+String.valueOf(m));
-            System.out.println("$"+m);
+            aumenarMoneda();
+
         }
     }
-
+    public void aumenarMoneda(){
+        m = m +100;
+        contador.setText("$"+String.valueOf(m));
+        System.out.println(m);
+    }
+    public void reducirMoneda(int n){
+        m = m -n;
+        contador.setText("$"+String.valueOf(m));
+        System.out.println(m);
+    }
+    public int getM() {
+        return m;
+    }
 
 }
-
