6e42c0a3ccfbdc13113148cc96f9d0a5d87e1dfd
Ignacio Cataldo <146041107+iCataldo@users.noreply.github.com>
2023-11-09 19:50:12 -0300

    Update ButtonBebida_Coca.java

--- src/Gui/ButtonBebida_Coca.java
+++ src/Gui/ButtonBebida_Coca.java
@@ -2,16 +2,13 @@ package Gui;
 import javax.swing.ImageIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
 public class ButtonBebida_Coca extends JButton {
 
     private Expendedor exp =new Expendedor(3);
     private Moneda mon;
     private Comprador com;
-    ImageIcon image;
-    public ButtonBebida_Coca( monButton m, PanelComprador beber, ImageIcon image){
-        super(image);
-        this.image = image;
+    public ButtonBebida_Coca( monButton m, PanelComprador beber){
+        super("Coca-Cola");
         this.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
