bf50b6c68aca39c8cd8074b07a90f309b1dec808
Ignacio Cataldo <146041107+iCataldo@users.noreply.github.com>
2023-11-09 19:35:33 -0300

    Update ButtonBebida_Sprite.java

--- src/Gui/ButtonBebida_Sprite.java
+++ src/Gui/ButtonBebida_Sprite.java
@@ -1,5 +1,5 @@
 package Gui;
-
+import javax.swing.ImageIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
@@ -7,8 +7,10 @@ public class ButtonBebida_Sprite extends JButton {
 
     private Moneda mon;
     private Comprador com;
-    public ButtonBebida_Sprite(monButton m, PanelComprador beber){
-        super("Sprite");
+    ImageIcon image;
+    public ButtonBebida_Sprite(monButton m, PanelComprador beber, ImageIcon image){
+        super(image);
+        this.image = image;
         this.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
