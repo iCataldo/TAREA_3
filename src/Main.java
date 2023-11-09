f65da4c784e8580004743ff69b9def7d7c3866c9
FMR08 <135428100+FMR08@users.noreply.github.com>
2023-11-09 19:51:17 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,7 @@
+import Gui.ventana;
+
 public class Main {
-    public static void main(String[] args) throws NoHayProductoException, PagoInsuficienteException, PagoIncorrectoException {
-        Expendedor exp = new Expendedor(3);
-        Moneda m = null;
-        Comprador c=null;
-        m = new Moneda1500();
-        c = new Comprador(m,Expendedor.SPRITE,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto()); //sprite $500
-        m = new Moneda1500();
-        c = new Comprador(m,Expendedor.SPRITE,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto()); //sprite $500
-        m = new Moneda1500();
-        c = new Comprador(m,Expendedor.SPRITE,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto()); //sprite $500
-        m = new Moneda1500();
-        c = new Comprador(m,Expendedor.SPRITE,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto()); //null $1500
-        m = new Moneda1500();
-        c = new Comprador(m,Expendedor.SPRITE,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto()); //null $1500
-        m = new Moneda1500();
-        c = new Comprador(m,Expendedor.SPRITE,exp);
-        System.out.println(c.queBebiste()+", "+c.cuantoVuelto()); //null $1500
-       }
+    public static void main(String[] args) {
+        ventana v = new ventana();
+    }
 }
