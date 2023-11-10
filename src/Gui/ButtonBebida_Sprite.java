1635c37dc34202524d2da0f32f1ecd2c19ef4143
FMR08 <135428100+FMR08@users.noreply.github.com>
2023-11-09 21:17:36 -0300

    Update ButtonBebida_Sprite.java

--- src/Gui/ButtonBebida_Sprite.java
+++ src/Gui/ButtonBebida_Sprite.java
@@ -7,22 +7,19 @@ public class ButtonBebida_Sprite extends JButton {
 
     private Moneda mon;
     private Comprador com;
-
     public ButtonBebida_Sprite(monButton m, PanelComprador beber){
+
         super("Sprite");
-       
         this.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
                 if (Expendedor.PRODUCTOS.PreSprite.precio<=m.getM()){
                     beber.getBeber().cabiotext("Sprite");
-                    beber.getComprar().definirprecio(Expendedor.PRODUCTOS.PreSprite.precio);
+                    beber.getComprar().definirproducto(2);
                 }
             }
         });
     }
-
-
-
 }
+
