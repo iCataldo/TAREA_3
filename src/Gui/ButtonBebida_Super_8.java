c7fc18fab7e84b8b347d81e377e6b6ecd707a084
FMR08 <135428100+FMR08@users.noreply.github.com>
2023-11-09 21:19:40 -0300

    Update ButtonBebida_Super_8.java

--- src/Gui/ButtonBebida_Super_8.java
+++ src/Gui/ButtonBebida_Super_8.java
@@ -7,22 +7,17 @@ public class ButtonBebida_Super_8 extends JButton {
     private Expendedor exp =new Expendedor(3);
     private Moneda mon;
     private Comprador com;
-
     public ButtonBebida_Super_8( monButton m, PanelComprador beber){
-        super("Super8");
-
+        super("Super_8");
         this.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
                 if (Expendedor.PRODUCTOS.PreSuper8.precio<=m.getM()){
                     beber.getBeber().cabiotext("Super 8");
-                    beber.getComprar().definirprecio(Expendedor.PRODUCTOS.PreSuper8.precio);
+                    beber.getComprar().definirproducto(4);
                 }
             }
         });
     }
-
-
-
 }
