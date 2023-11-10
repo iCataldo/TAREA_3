1886c2b89b0ca359311c5674d261be9f71e09c57
FMR08 <135428100+FMR08@users.noreply.github.com>
2023-11-09 21:09:41 -0300

    Update ButtonComprar.java

--- src/Gui/ButtonComprar.java
+++ src/Gui/ButtonComprar.java
@@ -5,18 +5,32 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ButtonComprar extends JButton {
-    private int precio;
-    public ButtonComprar(ButtonBeber beber,monButton mon){
+    private int tipo;
+    private Comprador com;
+    public ButtonComprar(ButtonBeber beber,monButton mon,Expendedor exp){
         super("Comprar!");
         this.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                beber.verdad();
-                mon.comprar(precio);
+
+                try {
+                    com = new Comprador(new Moneda1000(), tipo, exp);
+                } catch (NoHayProductoException ex) {
+                    throw new RuntimeException(ex);
+                } catch (PagoInsuficienteException ex) {
+                    throw new RuntimeException(ex);
+                } catch (PagoIncorrectoException ex) {
+                    throw new RuntimeException(ex);
+                }
+                if (com != null) {
+                    beber.verdad();
+                    mon.comprar(com.costo());
+                }
             }
         });
     }
-    public void definirprecio(int p){
-        precio=p;
+    public void definirproducto(int p){
+        tipo=p;
     }
+
 }
