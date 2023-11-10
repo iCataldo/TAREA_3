cb2dedf8b0047ccad46704fb368409082151089f
FMR08 <135428100+FMR08@users.noreply.github.com>
2023-11-09 21:15:21 -0300

    Update PanelExpendedor.java

--- src/Gui/PanelMatriz.java
+++ src/Gui/PanelMatriz.java
@@ -1,26 +1,24 @@
 package Gui;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class PanelMatriz extends JPanel {
-    public PanelMatriz( monButton mon, PanelComprador com){
+public class PanelExpendedor extends JPanel {
+    public PanelExpendedor( monButton mon, PanelComprador com){
         FlowLayout izquierda = new FlowLayout(FlowLayout.LEFT);
         this.setLayout(izquierda);
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(2,2));
+        panel.setLayout(new GridLayout(5,2));
+        panel.add(new ButtonBebida_Coca(mon,com));
+        panel.add(new ButtonBebida_Sprite(mon,com));
+        panel.add(new ButtonBebida_Snicker(mon,com));
+        panel.add(new ButtonBebida_Super_8(mon,com));
+        panel.add(new JButton("Nada_0"));
+        panel.add(new JButton("Nada_1"));
+        panel.add(new JButton("Nada_2"));
+        panel.add(new JButton("Nada_3"));
 
-        ImageIcon image1 = new ImageIcon("iconococa.png");
-        panel.add(new ButtonBebida_Coca(mon,com, image1));
-
-        ImageIcon image2 = new ImageIcon("iconosprite.png");
-        panel.add(new ButtonBebida_Sprite(mon,com, image2));
-
-        ImageIcon image3 = new ImageIcon("iconosnicker.png");
-        panel.add(new ButtonBebida_Snicker(mon,com, image3));
-
-        ImageIcon image4 = new ImageIcon("iconosuper8.png");
-        panel.add(new ButtonBebida_Super_8(mon,com, image4));
-        
         this.add(panel);
     }
 }
+
